29692d0b9c2143c032c777a9522092104930a5de
Yoana <yoanabarbie@icloud.com>
2024-06-17 16:58:34 +0200

    kickoff movie page with movie API

---
+++ js/main.js
@@ -0,0 +1,5 @@
+//TMD API IMPORT
+
+const API_KEY = '1cf50e6248dc270629e802686245c2c8';
+const BASE_URL = 'https://api.themoviedb.org/3';
+const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&';
